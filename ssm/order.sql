5cb6b1f6c9d4efc67ef5436cdd717559f08e286a
liuhuan <2806758770@qq.com>
2019-05-06 22:46:02 +0800

    dd

---
+++ ssm/order.sql
@@ -0,0 +1,326 @@
+﻿prompt PL/SQL Developer import file
+prompt Created on 2018年12月15日 by Administrator
+set feedback off
+set define off
+prompt Dropping MEMBER...
+drop table MEMBER cascade constraints;
+prompt Dropping PRODUCT...
+drop table PRODUCT cascade constraints;
+prompt Dropping ORDERS...
+drop table ORDERS cascade constraints;
+prompt Dropping TRAVELLER...
+drop table TRAVELLER cascade constraints;
+prompt Dropping ORDER_TRAVELLER...
+drop table ORDER_TRAVELLER cascade constraints;
+prompt Creating MEMBER...
+create table MEMBER
+(
+  id       VARCHAR2(32) default SYS_GUID() not null,
+  name     VARCHAR2(20),
+  nickname VARCHAR2(20),
+  phonenum VARCHAR2(20),
+  email    VARCHAR2(20)
+)
+tablespace SYSTEM
+  pctfree 10
+  pctused 40
+  initrans 1
+  maxtrans 255
+  storage
+  (
+    initial 64K
+    minextents 1
+    maxextents unlimited
+  );
+alter table MEMBER
+  add primary key (ID)
+  using index 
+  tablespace SYSTEM
+  pctfree 10
+  initrans 2
+  maxtrans 255
+  storage
+  (
+    initial 64K
+    minextents 1
+    maxextents unlimited
+  );
+
+prompt Creating PRODUCT...
+create table PRODUCT
+(
+  id            VARCHAR2(32) default SYS_GUID() not null,
+  productnum    VARCHAR2(50) not null,
+  productname   VARCHAR2(50),
+  cityname      VARCHAR2(50),
+  departuretime TIMESTAMP(6),
+  productprice  NUMBER,
+  productdesc   VARCHAR2(500),
+  productstatus INTEGER
+)
+tablespace SYSTEM
+  pctfree 10
+  pctused 40
+  initrans 1
+  maxtrans 255
+  storage
+  (
+    initial 64K
+    minextents 1
+    maxextents unlimited
+  );
+alter table PRODUCT
+  add primary key (ID)
+  using index 
+  tablespace SYSTEM
+  pctfree 10
+  initrans 2
+  maxtrans 255
+  storage
+  (
+    initial 64K
+    minextents 1
+    maxextents unlimited
+  );
+alter table PRODUCT
+  add constraint PRODUCT unique (ID, PRODUCTNUM)
+  using index 
+  tablespace SYSTEM
+  pctfree 10
+  initrans 2
+  maxtrans 255
+  storage
+  (
+    initial 64K
+    minextents 1
+    maxextents unlimited
+  );
+
+prompt Creating ORDERS...
+create table ORDERS
+(
+  id          VARCHAR2(32) default SYS_GUID() not null,
+  ordernum    VARCHAR2(20) not null,
+  ordertime   TIMESTAMP(6),
+  peoplecount INTEGER,
+  orderdesc   VARCHAR2(500),
+  paytype     INTEGER,
+  orderstatus INTEGER,
+  productid   VARCHAR2(32),
+  memberid    VARCHAR2(32)
+)
+tablespace SYSTEM
+  pctfree 10
+  pctused 40
+  initrans 1
+  maxtrans 255
+  storage
+  (
+    initial 64K
+    minextents 1
+    maxextents unlimited
+  );
+alter table ORDERS
+  add primary key (ID)
+  using index 
+  tablespace SYSTEM
+  pctfree 10
+  initrans 2
+  maxtrans 255
+  storage
+  (
+    initial 64K
+    minextents 1
+    maxextents unlimited
+  );
+alter table ORDERS
+  add unique (ORDERNUM)
+  using index 
+  tablespace SYSTEM
+  pctfree 10
+  initrans 2
+  maxtrans 255
+  storage
+  (
+    initial 64K
+    minextents 1
+    maxextents unlimited
+  );
+alter table ORDERS
+  add foreign key (PRODUCTID)
+  references PRODUCT (ID);
+alter table ORDERS
+  add foreign key (MEMBERID)
+  references MEMBER (ID);
+
+prompt Creating TRAVELLER...
+create table TRAVELLER
+(
+  id              VARCHAR2(32) default SYS_GUID() not null,
+  name            VARCHAR2(20),
+  sex             VARCHAR2(20),
+  phonenum        VARCHAR2(20),
+  credentialstype INTEGER,
+  credentialsnum  VARCHAR2(50),
+  travellertype   INTEGER
+)
+tablespace SYSTEM
+  pctfree 10
+  pctused 40
+  initrans 1
+  maxtrans 255
+  storage
+  (
+    initial 64K
+    minextents 1
+    maxextents unlimited
+  );
+alter table TRAVELLER
+  add primary key (ID)
+  using index 
+  tablespace SYSTEM
+  pctfree 10
+  initrans 2
+  maxtrans 255
+  storage
+  (
+    initial 64K
+    minextents 1
+    maxextents unlimited
+  );
+
+prompt Creating ORDER_TRAVELLER...
+create table ORDER_TRAVELLER
+(
+  orderid     VARCHAR2(32) not null,
+  travellerid VARCHAR2(32) not null
+)
+tablespace SYSTEM
+  pctfree 10
+  pctused 40
+  initrans 1
+  maxtrans 255
+  storage
+  (
+    initial 64K
+    minextents 1
+    maxextents unlimited
+  );
+alter table ORDER_TRAVELLER
+  add primary key (ORDERID, TRAVELLERID)
+  using index 
+  tablespace SYSTEM
+  pctfree 10
+  initrans 2
+  maxtrans 255
+  storage
+  (
+    initial 64K
+    minextents 1
+    maxextents unlimited
+  );
+alter table ORDER_TRAVELLER
+  add foreign key (ORDERID)
+  references ORDERS (ID);
+alter table ORDER_TRAVELLER
+  add foreign key (TRAVELLERID)
+  references TRAVELLER (ID);
+
+prompt Disabling triggers for MEMBER...
+alter table MEMBER disable all triggers;
+prompt Disabling triggers for PRODUCT...
+alter table PRODUCT disable all triggers;
+prompt Disabling triggers for ORDERS...
+alter table ORDERS disable all triggers;
+prompt Disabling triggers for TRAVELLER...
+alter table TRAVELLER disable all triggers;
+prompt Disabling triggers for ORDER_TRAVELLER...
+alter table ORDER_TRAVELLER disable all triggers;
+prompt Disabling foreign key constraints for ORDERS...
+alter table ORDERS disable constraint SYS_C004039;
+alter table ORDERS disable constraint SYS_C004040;
+prompt Disabling foreign key constraints for ORDER_TRAVELLER...
+alter table ORDER_TRAVELLER disable constraint SYS_C004043;
+alter table ORDER_TRAVELLER disable constraint SYS_C004044;
+prompt Loading MEMBER...
+insert into MEMBER (id, name, nickname, phonenum, email)
+values ('E61D65F673D54F68B0861025C69773DB', '张三', '小三', '18888888888', 'zs@163.com');
+commit;
+prompt 1 records loaded
+prompt Loading PRODUCT...
+insert into PRODUCT (id, productnum, productname, cityname, departuretime, productprice, productdesc, productstatus)
+values ('676C5BD1D35E429A8C2E114939C5685A', 'itcast-002', '北京三日游', '北京', to_timestamp('10-10-2018 10:10:00.000000', 'dd-mm-yyyy hh24:mi:ss.ff'), 1200, '不错的旅行', 1);
+insert into PRODUCT (id, productnum, productname, cityname, departuretime, productprice, productdesc, productstatus)
+values ('12B7ABF2A4C544568B0A7C69F36BF8B7', 'itcast-003', '上海五日游', '上海', to_timestamp('25-04-2018 14:30:00.000000', 'dd-mm-yyyy hh24:mi:ss.ff'), 1800, '魔都我来了', 0);
+insert into PRODUCT (id, productnum, productname, cityname, departuretime, productprice, productdesc, productstatus)
+values ('9F71F01CB448476DAFB309AA6DF9497F', 'itcast-001', '北京三日游', '北京', to_timestamp('10-10-2018 10:10:00.000000', 'dd-mm-yyyy hh24:mi:ss.ff'), 1200, '不错的旅行', 1);
+commit;
+prompt 3 records loaded
+prompt Loading ORDERS...
+insert into ORDERS (id, ordernum, ordertime, peoplecount, orderdesc, paytype, orderstatus, productid, memberid)
+values ('3081770BC3984EF092D9E99760FDABDE', '55555', to_timestamp('02-03-2018 12:00:00.000000', 'dd-mm-yyyy hh24:mi:ss.ff'), 2, '没什么', 0, 1, '9F71F01CB448476DAFB309AA6DF9497F', 'E61D65F673D54F68B0861025C69773DB');
+insert into ORDERS (id, ordernum, ordertime, peoplecount, orderdesc, paytype, orderstatus, productid, memberid)
+values ('0E7231DC797C486290E8713CA3C6ECCC', '12345', to_timestamp('02-03-2018 12:00:00.000000', 'dd-mm-yyyy hh24:mi:ss.ff'), 2, '没', 0, 1, '676C5BD1D35E429A8C2E114939C5685A', 'E61D65F673D54F68B0861025C69773DB');
+insert into ORDERS (id, ordernum, ordertime, peoplecount, orderdesc, paytype, orderstatus, productid, memberid)
+values ('5DC6A48DD4E94592AE904930EA866AFA', '54321', to_timestamp('02-03-2018 12:00:00.000000', 'dd-mm-yyyy hh24:mi:ss.ff'), 2, '没', 0, 1, '676C5BD1D35E429A8C2E114939C5685A', 'E61D65F673D54F68B0861025C69773DB');
+insert into ORDERS (id, ordernum, ordertime, peoplecount, orderdesc, paytype, orderstatus, productid, memberid)
+values ('2FF351C4AC744E2092DCF08CFD314420', '67890', to_timestamp('02-03-2018 12:00:00.000000', 'dd-mm-yyyy hh24:mi:ss.ff'), 2, '没', 0, 1, '12B7ABF2A4C544568B0A7C69F36BF8B7', 'E61D65F673D54F68B0861025C69773DB');
+insert into ORDERS (id, ordernum, ordertime, peoplecount, orderdesc, paytype, orderstatus, productid, memberid)
+values ('A0657832D93E4B10AE88A2D4B70B1A28', '98765', to_timestamp('02-03-2018 12:00:00.000000', 'dd-mm-yyyy hh24:mi:ss.ff'), 2, '没', 0, 1, '12B7ABF2A4C544568B0A7C69F36BF8B7', 'E61D65F673D54F68B0861025C69773DB');
+insert into ORDERS (id, ordernum, ordertime, peoplecount, orderdesc, paytype, orderstatus, productid, memberid)
+values ('E4DD4C45EED84870ABA83574A801083E', '11111', to_timestamp('02-03-2018 12:00:00.000000', 'dd-mm-yyyy hh24:mi:ss.ff'), 2, '没', 0, 1, '12B7ABF2A4C544568B0A7C69F36BF8B7', 'E61D65F673D54F68B0861025C69773DB');
+insert into ORDERS (id, ordernum, ordertime, peoplecount, orderdesc, paytype, orderstatus, productid, memberid)
+values ('96CC8BD43C734CC2ACBFF09501B4DD5D', '22222', to_timestamp('02-03-2018 12:00:00.000000', 'dd-mm-yyyy hh24:mi:ss.ff'), 2, '没', 0, 1, '12B7ABF2A4C544568B0A7C69F36BF8B7', 'E61D65F673D54F68B0861025C69773DB');
+insert into ORDERS (id, ordernum, ordertime, peoplecount, orderdesc, paytype, orderstatus, productid, memberid)
+values ('55F9AF582D5A4DB28FB4EC3199385762', '33333', to_timestamp('02-03-2018 12:00:00.000000', 'dd-mm-yyyy hh24:mi:ss.ff'), 2, '没', 0, 1, '9F71F01CB448476DAFB309AA6DF9497F', 'E61D65F673D54F68B0861025C69773DB');
+insert into ORDERS (id, ordernum, ordertime, peoplecount, orderdesc, paytype, orderstatus, productid, memberid)
+values ('CA005CF1BE3C4EF68F88ABC7DF30E976', '44444', to_timestamp('02-03-2018 12:00:00.000000', 'dd-mm-yyyy hh24:mi:ss.ff'), 2, '没', 0, 1, '9F71F01CB448476DAFB309AA6DF9497F', 'E61D65F673D54F68B0861025C69773DB');
+commit;
+prompt 9 records loaded
+prompt Loading TRAVELLER...
+insert into TRAVELLER (id, name, sex, phonenum, credentialstype, credentialsnum, travellertype)
+values ('3FE27DF2A4E44A6DBC5D0FE4651D3D3E', '张龙', '男', '13333333333', 0, '123456789009876543', 0);
+insert into TRAVELLER (id, name, sex, phonenum, credentialstype, credentialsnum, travellertype)
+values ('EE7A71FB6945483FBF91543DBE851960', '张小龙', '男', '15555555555', 0, '987654321123456789', 1);
+commit;
+prompt 2 records loaded
+prompt Loading ORDER_TRAVELLER...
+insert into ORDER_TRAVELLER (orderid, travellerid)
+values ('0E7231DC797C486290E8713CA3C6ECCC', '3FE27DF2A4E44A6DBC5D0FE4651D3D3E');
+insert into ORDER_TRAVELLER (orderid, travellerid)
+values ('2FF351C4AC744E2092DCF08CFD314420', '3FE27DF2A4E44A6DBC5D0FE4651D3D3E');
+insert into ORDER_TRAVELLER (orderid, travellerid)
+values ('3081770BC3984EF092D9E99760FDABDE', 'EE7A71FB6945483FBF91543DBE851960');
+insert into ORDER_TRAVELLER (orderid, travellerid)
+values ('55F9AF582D5A4DB28FB4EC3199385762', 'EE7A71FB6945483FBF91543DBE851960');
+insert into ORDER_TRAVELLER (orderid, travellerid)
+values ('5DC6A48DD4E94592AE904930EA866AFA', '3FE27DF2A4E44A6DBC5D0FE4651D3D3E');
+insert into ORDER_TRAVELLER (orderid, travellerid)
+values ('96CC8BD43C734CC2ACBFF09501B4DD5D', 'EE7A71FB6945483FBF91543DBE851960');
+insert into ORDER_TRAVELLER (orderid, travellerid)
+values ('A0657832D93E4B10AE88A2D4B70B1A28', '3FE27DF2A4E44A6DBC5D0FE4651D3D3E');
+insert into ORDER_TRAVELLER (orderid, travellerid)
+values ('CA005CF1BE3C4EF68F88ABC7DF30E976', 'EE7A71FB6945483FBF91543DBE851960');
+insert into ORDER_TRAVELLER (orderid, travellerid)
+values ('E4DD4C45EED84870ABA83574A801083E', 'EE7A71FB6945483FBF91543DBE851960');
+commit;
+prompt 9 records loaded
+prompt Enabling foreign key constraints for ORDERS...
+alter table ORDERS enable constraint SYS_C004039;
+alter table ORDERS enable constraint SYS_C004040;
+prompt Enabling foreign key constraints for ORDER_TRAVELLER...
+alter table ORDER_TRAVELLER enable constraint SYS_C004043;
+alter table ORDER_TRAVELLER enable constraint SYS_C004044;
+prompt Enabling triggers for MEMBER...
+alter table MEMBER enable all triggers;
+prompt Enabling triggers for PRODUCT...
+alter table PRODUCT enable all triggers;
+prompt Enabling triggers for ORDERS...
+alter table ORDERS enable all triggers;
+prompt Enabling triggers for TRAVELLER...
+alter table TRAVELLER enable all triggers;
+prompt Enabling triggers for ORDER_TRAVELLER...
+alter table ORDER_TRAVELLER enable all triggers;
+set feedback on
+set define on
+prompt Done.
